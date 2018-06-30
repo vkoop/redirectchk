a262055ce23907411d922148056dbca493672a95
Viktor Koop <viktor.koop@gmail.com>
2018-06-30 18:20:06 +0200

    move to utility extension

--- src/main/kotlin/de/vkoop/redirectchk/services/RedirectCheckImpl.kt
+++ src/main/kotlin/de/vkoop/redirectchk/services/RedirectCheckImpl.kt
@@ -1,13 +1,10 @@
 package de.vkoop.redirectchk.services
 
-import de.vkoop.redirectchk.data.ClientResponseWithUrl
 import de.vkoop.redirectchk.data.RedirectCheckRequest
 import de.vkoop.redirectchk.data.RedirectCheckResponse
+import de.vkoop.redirectchk.util.follow
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Flux
-import reactor.core.publisher.toFlux
 import java.net.URI
 
 @Component
@@ -15,7 +12,7 @@ class RedirectCheckImpl : RedirectCheck {
 
     override fun check(request: RedirectCheckRequest): RedirectCheckResponse {
         val client = WebClient.create();
-        val redirectSteps = follow(URI.create(request.callUrl), client).collectList().block()
+        val redirectSteps = client.follow(URI.create(request.callUrl)).collectList().block()
 
         val lastHop = redirectSteps.last()
         val firstHop = redirectSteps.first()
@@ -25,27 +22,5 @@ class RedirectCheckImpl : RedirectCheck {
         return response
     }
 
-    fun follow(requestUri: URI, client: WebClient): Flux<ClientResponseWithUrl> {
-        return client.get().uri(requestUri)
-                .exchange()
-                .toFlux()
-                .flatMap { response ->
-                    val redirectLocation = response.headers().header("Location")
-
-                    val responseFlux = Flux.just(ClientResponseWithUrl(response, requestUri.toString()))
-                    if (redirectLocation.isNotEmpty() && response.statusCode().is3xxRedirection) {
-                        val newUrl = redirectLocation.get(0)
-                        var newURI = URI.create(newUrl)
-
-                        if (!newURI.isAbsolute) {
-                            newURI = requestUri.resolve(newURI)
-                        }
-
-                        Flux.concat(responseFlux, follow(newURI, client))
-                    } else {
-                        responseFlux
-                    }
-                }
-    }
 }
 
---
+++ src/main/kotlin/de/vkoop/redirectchk/util/WebClientExtension.kt
@@ -0,0 +1,31 @@
+package de.vkoop.redirectchk.util
+
+import de.vkoop.redirectchk.data.ClientResponseWithUrl
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
+import reactor.core.publisher.toFlux
+import java.net.URI
+
+
+fun WebClient.follow(requestUriString: URI): Flux<ClientResponseWithUrl> {
+    return get().uri(requestUriString)
+            .exchange()
+            .toFlux()
+            .flatMap { response ->
+                val redirectLocation = response.headers().header("Location")
+
+                val responseFlux = Flux.just(ClientResponseWithUrl(response, requestUriString.toString()))
+                if (redirectLocation.isNotEmpty() && response.statusCode().is3xxRedirection) {
+                    val newUrl = redirectLocation.get(0)
+                    var newURI = URI.create(newUrl)
+
+                    if (!newURI.isAbsolute) {
+                        newURI = requestUriString.resolve(newURI)
+                    }
+
+                    Flux.concat(responseFlux, follow(newURI))
+                } else {
+                    responseFlux
+                }
+            }
+}
