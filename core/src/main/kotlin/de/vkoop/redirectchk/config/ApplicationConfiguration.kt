f5ecdb4267c12ef099fda19ae27532a74849c76d
Viktor Koop <viktor.koop@gmail.com>
2018-07-08 22:53:15 +0200

    fix unit tests

---
+++ core/src/main/kotlin/de/vkoop/redirectchk/config/ApplicationConfiguration.kt
@@ -0,0 +1,8 @@
+package de.vkoop.redirectchk.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@ComponentScan("de.vkoop.redirectchk")
+@Configuration
+class ApplicationConfiguration
--- core/src/test/kotlin/de/vkoop/redirectchk/services/RedirectCheckStrategyImplTest.kt
+++ core/src/test/kotlin/de/vkoop/redirectchk/services/RedirectCheckStrategyImplTest.kt
@@ -2,17 +2,19 @@ package de.vkoop.redirectchk.services
 
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import com.github.tomakehurst.wiremock.junit.WireMockRule
+import de.vkoop.redirectchk.config.ApplicationConfiguration
 import de.vkoop.redirectchk.data.RedirectCheckRequest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import javax.inject.Inject
 import kotlin.test.assertTrue
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest( classes = [ApplicationConfiguration::class])
 class RedirectCheckStrategyImplTest {
 
     private val port = 8089
